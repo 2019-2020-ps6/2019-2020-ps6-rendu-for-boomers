8a8074e47e4f978427c51dbbf0d0d4a3d6d3194d
Matéo <mateo.borlat@gmail.com>
2020-03-17 20:47:08 +0100

    accueil + menu

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  { path: 'accueil', 
+    component: AccueilComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
