ec5957678e6bd1abb7b1aa5edc3342c07755fe78
laudienp <paul.laudien@etu.unice.fr>
2020-04-21 12:01:10 +0200

    menu button work

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CloseReglageService } from '../close-reglage.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -8,13 +9,18 @@ import { CloseReglageService } from '../close-reglage.service';
 })
 export class HeaderComponent implements OnInit
 {
-    constructor()
+    constructor(private router: Router)
     {
 
     }
 
     ngOnInit(): void 
     {
-        
+
+    }
+
+    goToMenu(): void
+    {
+        this.router.navigate(['/menu']);
     }
 }
