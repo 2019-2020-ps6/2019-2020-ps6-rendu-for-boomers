78273328a192bf34ce8b726c47912ce69930b818
LanouxNicolas <nicolas.lanoux@etu.univ-cotedazur.fr>
2020-04-25 11:16:08 +0200

    modifier quiz

--- front-end/src/app/editor/modify/modify.component.ts
+++ front-end/src/app/editor/modify/modify.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CloseReglageService } from '../../close-reglage.service';
 import { Subject, Subscription } from 'rxjs';
 import { ReglageService } from 'src/services/reglage.service';
+import { QuizService } from '../../../services/quiz.service';
+import { Quiz } from 'src/models/quiz.model';
+import { Question } from 'src/models/question.model';
 import * as $ from 'jquery';
 
 
@@ -10,29 +14,48 @@ import * as $ from 'jquery';
   templateUrl: './modify.component.html',
   styleUrls: ['./modify.component.scss']
 })
+
+
 export class ModifyComponent implements OnInit {
-  buttonIsActivated: boolean = false;
-  buttonObserver: Subscription;
+  public quizList: Quiz[];
+  public quiz: Quiz;
+
+  public pageTitle: string = "Modifier un quiz";
   public valueContrast: number;
   public height: number;
-  public pageTitle: string = "Modifier un quiz";
 
-  constructor(private closeReglageService: CloseReglageService, public reglageService: ReglageService) { 
-   
+  buttonIsActivated: boolean = false;
+  buttonObserver: Subscription;
+
+
+  constructor(private router: Router, public quizService: QuizService, private closeReglageService: CloseReglageService)
+  {
+      this.quizService.quizzes$.subscribe((quizzes: Quiz[]) =>
+      {
+          this.quizList = quizzes;
+          this.quiz = quizzes[0];
+      })
   }
 
   ngOnInit(): void {
-    this.buttonObserver = this.closeReglageService.closeReglage$.subscribe(() => {
-      this.buttonIsActivated = false;
-    });
-    this.closeReglageService.update();
-
-    this.reglageService.valueContrast.subscribe((value: number) => 
-    {
-      this.valueContrast = value;
-    })
-    this.reglageService.updateContrast(this.valueContrast);
+      this.buttonObserver = this.closeReglageService.closeReglage$.subscribe(() => {
+          this.buttonIsActivated = false;
+        });
+        this.closeReglageService.update();
   }
+
+  quizSelected(selected: boolean)
+  {
+      console.log('event received from child ', selected);  
+  }
+
+  editQuiz(quiz: Quiz)
+  {
+      this.router.navigate(['/quizz/'+ quiz.name]);
+  }
+
+
+
 
   goToReglageOrQuit(): void {
     if ( this.buttonIsActivated == false ) this.buttonIsActivated = true;
