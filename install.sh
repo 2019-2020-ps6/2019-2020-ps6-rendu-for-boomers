2b49c5fcdbb16463b73e99a6435afc18f52c0c21
laudienp <paul.laudien@etu.unice.fr>
2020-05-10 22:45:39 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 npm install --prefix back-end
+npm install --prefix front-end
+npm install -g @angular/cli --prefix front-end
+npm install ng2-charts --prefix front-end
+npm install chart.js --prefix front-end
