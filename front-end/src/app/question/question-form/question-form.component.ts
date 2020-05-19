766dceef335f66484c865fecb2c9a2b14f5a1d7b
Matéo <mateo.borlat@gmail.com>
2020-05-19 13:54:27 +0200

    pop up modif question / reponse

--- front-end/src/app/question/question-form/question-form.component.ts
+++ front-end/src/app/question/question-form/question-form.component.ts
@@ -16,6 +16,11 @@ export class QuestionFormComponent implements OnInit
 
     public questionForm: FormGroup;
 
+    public questionIsEmpty: boolean;
+    public answerIsEmpty: boolean;
+    public isThereAnswer: boolean;
+    public isThereOneGoodAnswer: boolean;
+
     constructor(public formBuilder: FormBuilder, private quizSerivce: QuizService)
     {
         this.initializeQuestionForm();
@@ -54,11 +59,53 @@ export class QuestionFormComponent implements OnInit
 
     addQuestion()
     {
-        if(this.questionForm.valid)
+        if(this.questionForm.valid && this.answers.length!=0 && this.checkIfAllAnswersAreFilled() == true && this.checkIfOneAnswerIsTrue() == true)
         {
             const question = this.questionForm.getRawValue() as Question;
             this.quizSerivce.addQuestion(this.quiz, question);
             this.initializeQuestionForm();
         }
+        else if(this.questionForm.valid != true)
+            this.setQuestionIsEmpty(true);
+        else if(this.answers.length == 0)
+            this.setIsThereAnswer(true);
+        else if(this.checkIfAllAnswersAreFilled() != true)
+            this.setAnswerIsEmpty(true);
+        else if(this.checkIfOneAnswerIsTrue() != true) {
+            this.setIsThereOneGoodAnswer(true);
+        }
+    }
+
+    checkIfAllAnswersAreFilled(): boolean{
+        for(let answer of this.answers.value){
+            if(answer.value == ""){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkIfOneAnswerIsTrue(): boolean {
+        for(let answer of this.answers.value){
+            if(answer.isCorrect == true)
+                return true;
+        }
+        return false;
+    }
+
+    setQuestionIsEmpty(bool: boolean){
+        this.questionIsEmpty = bool;
+    }
+
+    setAnswerIsEmpty(bool: boolean){
+        this.answerIsEmpty = bool;
+    }
+
+    setIsThereAnswer(bool: boolean){
+        this.isThereAnswer = bool;
+    }
+
+    setIsThereOneGoodAnswer(bool: boolean){
+        this.isThereOneGoodAnswer = bool;
     }
 }
